6793568300369d8acff0a036977ba21fe18e9dad
Christerle Nounoute <christerlesn@gmail.com>
2020-09-24 17:39:11 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,13 +1,13 @@
 class Cat
 attr_accessor :name
 
-def meow=(cat_meow)
-  cat_meow = puts "meow!"
-  @meow = cat_meow
-end
+# def meow=(cat_meow)
+#   cat_meow = puts "meow!"
+#   @meow = cat_meow
+# end
 
 def meow
-  @meow
+  puts "meow!"
 end
 
 end
